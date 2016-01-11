77ee33b03abb9bd7d5413dfab380b9d295f2541e
tanliqing <qing1115812681@gmail.com>
2016-01-11 11:06:00 +0800

    component

--- linux-script/component-generation.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# 前端组件生成
-
-component-name="sow-livestock"
-component-catalog="/Users/tanliqingcn/Downloads"
-component-type=""
-component-type=
---
+++ linux-script/component.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# 前端组件生成
+
+component_name="sow-livestock"
+component_project_folder="/Users/tanliqingcn/Downloads"
+
+# 组件类型
+component_type=""
+# 只是hbs文件
+component_type_h="hbs"
+# 只是hbs与coffee文件
+component_type_hc="hbsCoffee"
+# 只是hbs、coffee、scss文件
+component_type_hcs="hbsCoffeeScss"
+# 仿照另外一个组件
+component_type_comp="comp"
+
+# 文件类型
+component_file_coffee="coffee"
+component_file_hbs="hbs"
+component_file_scss="scss"

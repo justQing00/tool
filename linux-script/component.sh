8d23ec4cf2c061e057449d0f32dfb52a1d3ad554
tanliqing <qing1115812681@gmail.com>
2016-01-11 15:43:40 +0800

    basic create file

--- linux-script/component.sh
+++ linux-script/component.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
-
 # 前端组件生成
 
-component_name="sow-livestock"
-component_project_folder="/Users/tanliqingcn/Downloads"
+# echo 输出颜色值
+color_blue="\033[36m"
+color_white="\033[37m"
+color_base=$color_blue
+color_other=$color_white
+
+# 组件的基本信息
+user_folder="~/"
+project_folder=$user_folder"SoftWare/gitProject/"
+project_name=""
+component_path=""
+component_name=""
+component_target=""
 
 # 组件类型
 component_type=""
@@ -17,6 +27,61 @@ component_type_hcs="hbsCoffeeScss"
 component_type_comp="comp"
 
 # 文件类型
-component_file_coffee="coffee"
-component_file_hbs="hbs"
-component_file_scss="scss"
+component_file_coffee="view.coffee"
+component_file_hbs="view.hbs"
+component_file_scss="view.scss"
+
+# 创建hbs文件
+creat_h()
+{
+  className_hbs=""
+  touch $component_file_hbs
+}
+
+# 创建hbs、coffee文件
+creat_hc()
+{
+  creat_h
+  className_coffee=""
+  touch $component_file_coffee
+}
+
+# 创建hbs、coffee、scss文件
+creat_hcs()
+{
+  creat_hc
+  className_scss=""
+  touch $component_file_scss
+}
+
+executionCreat()
+{
+  echo "Will create file now"
+}
+
+case "$1" in
+  'hbs')
+    component_type=$component_type_h
+    executionCreat
+    ;;
+  'hbsCoffee')
+    component_type=$component_type_hc
+    executionCreat
+    ;;
+  'hbsCoffeeScss')
+    component_type=$component_type_hcs
+    executionCreat
+    ;;
+  'comp')
+    component_type=$component_type_comp
+    executionCreat
+    ;;
+  *)
+    echo "$color_base""Commands: "
+    echo "  hbs: create hbs file."
+    echo "  hbsCoffee: create hbs and coffee file."
+    echo "  hbsCoffeeScss: create hbs、coffee and scss file."
+    echo "  comp: create component that like another one."
+    exit 1
+    ;;
+esac

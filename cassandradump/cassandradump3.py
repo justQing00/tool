26c8d668379495d3ca05e1a63b8b7738bd58dedf
tanliqing <qing1115812681@gmail.com>
2016-03-29 10:19:14 +0800

    import file

--- cassandradump/cassandradump3.py
+++ cassandradump/cassandradump3.py
@@ -1,1 +1,2 @@
-python cassandradump.py --host 127.0.0.1 --import-file dump.cql --keyspace neverest
+python cassandradump.py --host 127.0.0.1 --import-file xaa.cql --keyspace neverest
+python cassandradump.py --host 127.0.0.1 --import-file xab.cql --keyspace neverest
